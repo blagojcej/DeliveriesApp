47b1552a8c600610bde15beca49c71b4bd5c97f8
blagojcej <blagojcejovanoski@yahoo.com>
2019-10-31 22:53:26 +0100

    Added Login and Register Activity.

---
+++ DeliveriesApp/MainActivity.cs
@@ -0,0 +1,65 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace DeliveriesApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private EditText emailEditText;
+        private EditText passwordEditText;
+        private Button signInButton;
+        private Button registerButton;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            emailEditText = FindViewById<EditText>(Resource.Id.emailEditText);
+            passwordEditText = FindViewById<EditText>(Resource.Id.passwordEditText);
+            signInButton = FindViewById<Button>(Resource.Id.signInButton);
+            registerButton = FindViewById<Button>(Resource.Id.registerButton);
+
+            signInButton.Click += SignInButton_Click;
+            registerButton.Click += RegisterButton_Click;
+        }
+
+        private void RegisterButton_Click(object sender, System.EventArgs e)
+        {
+            var intent = new Intent(this, typeof(RegisterActivity));
+
+            //Pass information to register activity
+            intent.PutExtra("email", emailEditText.Text);
+
+            StartActivity(intent);
+        }
+
+        private void SignInButton_Click(object sender, System.EventArgs e)
+        {
+            
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+	}
+}
+
---
+++ DeliveriesApp/RegisterActivity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace DeliveriesApp
+{
+    [Activity(Label = "RegisterActivity")]
+    public class RegisterActivity : Activity
+    {
+        private EditText registerEmailEditText, registerPasswordEditText, registerConfirmPasswordEditText;
+        private Button registerUserButton;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.register);
+
+            registerEmailEditText = FindViewById<EditText>(Resource.Id.registerEmailEditText);
+            registerPasswordEditText = FindViewById<EditText>(Resource.Id.registerPasswordEditText);
+            registerConfirmPasswordEditText = FindViewById<EditText>(Resource.Id.registerConfirmPasswordEditText);
+            registerUserButton = FindViewById<Button>(Resource.Id.registerUserButton);
+
+            registerUserButton.Click += RegisterUserButton_Click;
+
+            string email = Intent.GetStringExtra("email");
+            registerEmailEditText.Text = email;
+        }
+
+        private void RegisterUserButton_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
