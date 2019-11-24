9c94abe8728fd182b283c582792f48f3c55797cf
blagojcej <blagojcejovanoski@yahoo.com>
2019-11-24 14:50:39 +0100

    Implementing Maps in Android (Not Tested, requires Android Device)

--- DeliveriesApp/NewDeliveryActivity.cs
+++ DeliveriesApp/NewDeliveryActivity.cs
@@ -5,6 +5,9 @@ using System.Text;
 
 using Android.App;
 using Android.Content;
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
+using Android.Locations;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
@@ -14,10 +17,35 @@ using DeliveriesApp.Shared.Model;
 namespace DeliveriesApp
 {
     [Activity(Label = "NewDeliveryActivity")]
-    public class NewDeliveryActivity : Activity
+    public class NewDeliveryActivity : Activity, IOnMapReadyCallback, ILocationListener
     {
         private Button saveButton;
         private EditText packageNameEditText;
+        private MapFragment destinationMapFragment;
+        private double latitute, longitude;
+        //Location Manager handle changes in the users location and let us know about that changes
+        private LocationManager locationManager;
+        private GoogleMap gMap;
+        private Location currentLocation;
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+
+            var destinationLocation = gMap.CameraPosition.Target;
+            Delivery newDelivery = new Delivery()
+            {
+                Name = packageNameEditText.Text.Trim(),
+                Status = 0,
+                OriginLatitude = currentLocation.Latitude,
+                OriginLongitude = currentLocation.Longitude,
+                DestinationLatitude = destinationLocation.Latitude,
+                DestinationLongitude = destinationLocation.Longitude
+            };
+
+            Delivery.InsertDelivery(newDelivery);
+        }
+
+        #region Override
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -28,19 +56,108 @@ namespace DeliveriesApp
 
             saveButton = FindViewById<Button>(Resource.Id.saveButton);
             packageNameEditText = FindViewById<EditText>(Resource.Id.packageNameEditText);
+            destinationMapFragment = FragmentManager.FindFragmentById<MapFragment>(Resource.Id.destinationMapFragment);
+
+            //Latitude and logitude may not have values yet
+            //mapFragment.GetMapAsync(this);
 
             saveButton.Click += SaveButton_Click;
+
+            //Moved to OnResume
+            ////Location service is system service which always interact with hardware
+            //locationManager = GetSystemService(LocationService) as LocationManager;
+            //string provider = LocationManager.GpsProvider;
+
+            ////Check if provider is available, does device has gps and does gps i turned on
+            //if (locationManager.IsProviderEnabled(provider))
+            //{
+            //    locationManager.RequestLocationUpdates(provider, 5000, 1, this);
+            //}
         }
 
-        private void SaveButton_Click(object sender, EventArgs e)
+        protected override void OnPause()
         {
-            Delivery newDelivery=new Delivery()
-            {
-                Name = packageNameEditText.Text.Trim(),
-                Status = 0
-            };
-
-            Delivery.InsertDelivery(newDelivery);
+            base.OnPause();
+            //Stop listening for location changes whan app goes in background
+            locationManager.RemoveUpdates(this);
         }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            //Start listening for location changes when app is activated again
+            //Location service is system service which always interact with hardware
+            locationManager = GetSystemService(LocationService) as LocationManager;
+            string provider = LocationManager.GpsProvider;
+
+            //Check if provider is available, does device has gps and does gps i turned on
+            if (locationManager.IsProviderEnabled(provider))
+            {
+                locationManager.RequestLocationUpdates(provider, 5000, 1, this);
+            }
+
+            //Get initial location
+            //Set new provider (NetworkProvider), not very accured, just to set initial location
+            var location = locationManager.GetLastKnownLocation(LocationManager.NetworkProvider);
+            latitute = location.Latitude;
+            longitude = location.Longitude;
+            destinationMapFragment.GetMapAsync(this);
+        }
+
+        #endregion Override
+
+        #region IOnMapReadyCallback
+
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            gMap = googleMap;
+
+            //Add pin to map
+            MarkerOptions marker=new MarkerOptions();
+            marker.SetPosition(new LatLng(latitute, longitude));
+            marker.SetTitle("Your Location");
+            googleMap.AddMarker(marker);
+
+            //CameraPosition.Builder builder = CameraPosition.InvokeBuilder();
+            //builder.Target(marker.Position);
+            //builder.Zoom(18);
+            //builder.Bearing(155);
+            //builder.Tilt(65);
+            //CameraPosition cameraPosition = builder.Build();
+            //CameraUpdate cameraUpdate = CameraUpdateFactory.NewCameraPosition(cameraPosition);
+            //googleMap.MoveCamera(cameraUpdate);
+
+            googleMap.MoveCamera(CameraUpdateFactory.NewLatLng(new LatLng(latitute, longitude)));
+        }
+
+        #endregion IOnMapReadyCallback
+
+        #region ILocationListener
+
+        public void OnLocationChanged(Location location)
+        {
+            latitute = location.Latitude;
+            longitude = location.Longitude;
+            //To be sure latitude and logitude are set
+            destinationMapFragment.GetMapAsync(this);
+            currentLocation = location;
+        }
+
+        public void OnProviderDisabled(string provider)
+        {
+            
+        }
+
+        public void OnProviderEnabled(string provider)
+        {
+            
+        }
+
+        public void OnStatusChanged(string provider, Availability status, Bundle extras)
+        {
+            
+        }
+
+        #endregion ILocationListener
     }
 }
